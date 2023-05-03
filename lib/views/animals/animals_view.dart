2898fc2afdd85c9bb03b802e2ec27c57066ecc58
renankanu <renankanu@gmail.com>
2023-05-03 17:11:14 -0300

    feature: add imagens sun and clouds in animals list

--- lib/views/animals/animals_view.dart
+++ lib/views/animals/animals_view.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../routes/app_routes.dart';
+import '../../shared/components/cloud_sun.dart';
+import '../../shared/images.dart';
 
 class AnimalsView extends StatelessWidget {
   const AnimalsView({super.key});
@@ -15,23 +17,40 @@ class AnimalsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: const BaseAppBar(label: 'Animais'),
-      body: GridView(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 60,
-        ),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-          childAspectRatio: 1.1,
-        ),
+      body: Stack(
         children: [
-          for (var animal in Animals.values)
-            ButtonAnimal(
-                animals: animal, onTap: () => goToView(context, animal))
+          const CloudSun(
+            top: 0,
+            left: 6,
+            height: 80,
+            icon: AppImages.sun,
+          ),
+          CloudSun(
+            top: height * 0.2,
+            right: -20,
+            height: 70,
+            icon: AppImages.cloudThree,
+          ),
+          GridView(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 60,
+            ),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+              childAspectRatio: 1.1,
+            ),
+            children: [
+              for (var animal in Animals.values)
+                ButtonAnimal(
+                    animals: animal, onTap: () => goToView(context, animal))
+            ],
+          ),
         ],
       ),
     );
